cc99e26168c0da1ac1651eaab8d26da5858af9c7
PHONGRAWEE YAEMMISRI <phongrawee_y@cmu.ac.th>
2018-09-04 14:39:32 +0700

    Add √ and 1/x

--- CPE200Lab1/CPE200Lab1/MainForm.cs
+++ CPE200Lab1/CPE200Lab1/MainForm.cs
@@ -18,6 +18,7 @@ namespace CPE200Lab1
         private bool isAfterEqual;
         private string firstOperand;
         private string operate;
+        CalculatorEngine engine;
 
         private void resetAll()
         {
@@ -28,48 +29,49 @@ namespace CPE200Lab1
             isAfterEqual = false;
         }
 
-        private string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
-        {
-            switch(operate)
-            {
-                case "+":
-                    return (Convert.ToDouble(firstOperand) + Convert.ToDouble(secondOperand)).ToString();
-                case "-":
-                    return (Convert.ToDouble(firstOperand) - Convert.ToDouble(secondOperand)).ToString();
-                case "X":
-                    return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
-                case "÷":
-                    // Not allow devide be zero
-                    if(secondOperand != "0")
-                    {
-                        double result;
-                        string[] parts;
-                        int remainLength;
+        /*private string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
+		{
+		switch(operate)
+		{
+		case "+":
+		return (Convert.ToDouble(firstOperand) + Convert.ToDouble(secondOperand)).ToString();
+		case "-":
+		return (Convert.ToDouble(firstOperand) - Convert.ToDouble(secondOperand)).ToString();
+		case "X":
+		return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
+		case "÷":
+		// Not allow devide be zero
+		if(secondOperand != "0")
+		{
+		double result;
+		string[] parts;
+		int remainLength;
 
-                        result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
-                        // split between integer part and fractional part
-                        parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
-                        if(parts[0].Length > maxOutputSize)
-                        {
-                            return "E";
-                        }
-                        // calculate remaining space for fractional part.
-                        remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        return result.ToString("N" + remainLength);
-                    }
-                    break;
-                case "%":
-                    //your code here
-                    break;
-            }
-            return "E";
-        }
+		result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
+		// split between integer part and fractional part
+		parts = result.ToString().Split('.');
+		// if integer part length is already break max output, return error
+		if(parts[0].Length > maxOutputSize)
+		{
+		return "E";
+		}
+		// calculate remaining space for fractional part.
+		remainLength = maxOutputSize - parts[0].Length - 1;
+		// trim the fractional part gracefully. =
+		return result.ToString("N" + remainLength);
+		}
+		break;
+		case "%":
+		//your code here
+		break;
+		}
+		return "E";
+		}*/
 
         public MainForm()
         {
             InitializeComponent();
+            engine = new CalculatorEngine();
 
             resetAll();
         }
@@ -88,13 +90,13 @@ namespace CPE200Lab1
             {
                 lblDisplay.Text = "0";
             }
-            if(lblDisplay.Text.Length is 8)
+            if (lblDisplay.Text.Length is 8)
             {
                 return;
             }
             isAllowBack = true;
             string digit = ((Button)sender).Text;
-            if(lblDisplay.Text is "0")
+            if (lblDisplay.Text is "0")
             {
                 lblDisplay.Text = "";
             }
@@ -123,11 +125,18 @@ namespace CPE200Lab1
                     isAfterOperater = true;
                     break;
                 case "%":
-                    // your code here
+                    lblDisplay.Text = engine.calculate(operate, firstOperand, lblDisplay.Text);
+                    break;
+                case "√":
+                    lblDisplay.Text = engine.calculate(operate, lblDisplay.Text, lblDisplay.Text);
+                    break;
+                case "1/X":
+                    lblDisplay.Text = engine.calculate(operate, lblDisplay.Text, lblDisplay.Text);
                     break;
             }
             isAllowBack = false;
         }
+
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
@@ -136,7 +145,7 @@ namespace CPE200Lab1
                 return;
             }
             string secondOperand = lblDisplay.Text;
-            string result = calculate(operate, firstOperand, secondOperand);
+            string result = engine.calculate(operate, firstOperand, secondOperand);
             if (result is "E" || result.Length > 8)
             {
                 lblDisplay.Text = "Error";
@@ -184,10 +193,11 @@ namespace CPE200Lab1
             {
                 return;
             }
-            if(lblDisplay.Text[0] is '-')
+            if (lblDisplay.Text[0] is '-')
             {
                 lblDisplay.Text = lblDisplay.Text.Substring(1, lblDisplay.Text.Length - 1);
-            } else
+            }
+            else
             {
                 lblDisplay.Text = "-" + lblDisplay.Text;
             }
@@ -212,16 +222,16 @@ namespace CPE200Lab1
             {
                 return;
             }
-            if(lblDisplay.Text != "0")
+            if (lblDisplay.Text != "0")
             {
                 string current = lblDisplay.Text;
                 char rightMost = current[current.Length - 1];
-                if(rightMost is '.')
+                if (rightMost is '.')
                 {
                     hasDot = false;
                 }
                 lblDisplay.Text = current.Substring(0, current.Length - 1);
-                if(lblDisplay.Text is "" || lblDisplay.Text is "-")
+                if (lblDisplay.Text is "" || lblDisplay.Text is "-")
                 {
                     lblDisplay.Text = "0";
                 }
